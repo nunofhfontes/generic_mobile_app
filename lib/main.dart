9376b6937ec36e10585a9e460f7bc1b8220b229a
nunofontes <nunof.h.fontes@gmail.com>
2021-08-21 00:28:24 +0100

    clean home page screen with placeholder logo

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,20 @@ class GenericApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
+      theme: ThemeData(
+          scaffoldBackgroundColor: const Color(0xFFF0F2F6), // #F0F2F6
+          primaryColor: const Color(0xFFF0F2F6)
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Center(
-            child: Text("Generic App Logo",
-              textAlign: TextAlign.center,
-            ),
+          title: Image.asset(
+            "assets/images/github_logo.png",
+            fit: BoxFit.contain,
+            height: 72,
           ),
+          centerTitle: true,
+          toolbarHeight: 88,
+          elevation: 0,
         ),
       ),
     );
