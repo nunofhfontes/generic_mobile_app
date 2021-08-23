48a25e716a7962b1c08901c1b506c3934c392ee5
nunofontes <nunof.h.fontes@gmail.com>
2021-08-23 18:55:51 +0100

    Added a GestureDetector for closing keyboard when clicking outside inputs

--- lib/main.dart
+++ lib/main.dart
@@ -6,56 +6,52 @@ void main() {
 }
 
 class GenericApp extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       theme: ThemeData(
           scaffoldBackgroundColor: const Color(0xFFF0F2F6), // #F0F2F6
-          primaryColor: const Color(0xFFF0F2F6)
-      ),
-      home:
-
-      Scaffold(
-        appBar: AppBar(
-          title: Image.asset(
-            "assets/images/github_transparent_logoicon.png",
-            fit: BoxFit.contain,
-            height: 72,
+          primaryColor: const Color(0xFFF0F2F6)),
+      home: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Image.asset(
+              "assets/images/github_transparent_logoicon.png",
+              fit: BoxFit.contain,
+              height: 72,
+            ),
+            centerTitle: true,
+            toolbarHeight: 88,
+            elevation: 0,
           ),
-          centerTitle: true,
-          toolbarHeight: 88,
-          elevation: 0,
-        ),
-        body: new Container(
-          // color: Color(0xff258DED),
-          margin: const EdgeInsets.all(16.0),
-          alignment: Alignment.center,
-          child: new Column(
-            children: <Widget>[
-              new Container(
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'User Name'
+          body: new Container(
+            // color: Color(0xff258DED),
+            margin: const EdgeInsets.all(16.0),
+            alignment: Alignment.center,
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'User Name'
+                    ),
                   ),
                 ),
-              ),
-              new Container(
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Password'
+                new Container(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Password'
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
