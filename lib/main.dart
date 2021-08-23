f24d48604eae12339234184d0bc36a237b80d05b
nunofontes <nunof.h.fontes@gmail.com>
2021-08-23 18:54:50 +0100

    Added inputs on home Container

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,9 @@ class GenericApp extends StatelessWidget {
           scaffoldBackgroundColor: const Color(0xFFF0F2F6), // #F0F2F6
           primaryColor: const Color(0xFFF0F2F6)
       ),
-      home: Scaffold(
+      home:
+
+      Scaffold(
         appBar: AppBar(
           title: Image.asset(
             "assets/images/github_transparent_logoicon.png",
@@ -26,6 +28,31 @@ class GenericApp extends StatelessWidget {
           centerTitle: true,
           toolbarHeight: 88,
           elevation: 0,
+        ),
+        body: new Container(
+          // color: Color(0xff258DED),
+          margin: const EdgeInsets.all(16.0),
+          alignment: Alignment.center,
+          child: new Column(
+            children: <Widget>[
+              new Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'User Name'
+                  ),
+                ),
+              ),
+              new Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Password'
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
