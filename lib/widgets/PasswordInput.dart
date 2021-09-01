09f8d033eab59074def61aebeda3e0f231657170
nunofontes <nunof.h.fontes@gmail.com>
2021-09-01 10:09:20 +0100

    Extracted Password input on a separate Widget

---
+++ lib/widgets/PasswordInput.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class PasswordInput extends StatelessWidget {
+  const PasswordInput({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: EdgeInsets.symmetric(vertical: 8.0),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            labelText: 'Password',
+            labelStyle: TextStyle(color: Color(0xFF8091A5)),
+            prefixIcon: Icon(
+              Icons.lock_outlined,
+              color: Color(0xFF8091A5),
+            ),
+            filled: true,
+            fillColor: Color(0xFFCDD5DD),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1, color: Color(0xFFCDD5DD)),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 2, color: Colors.blue),
+              borderRadius: BorderRadius.circular(4),
+            )
+        ),
+
+      ),
+    );
+  }
+}
