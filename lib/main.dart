e5dc122be61c6e0a81c18731cb60a84708507884
nunofontes <nunof.h.fontes@gmail.com>
2021-09-06 18:44:30 +0100

    Fixing scrolling Column

--- lib/main.dart
+++ lib/main.dart
@@ -31,18 +31,16 @@ class GenericApp extends StatelessWidget {
             child: ListView(
               children: [
                 DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Color(0xFF9AC433)
-                  ),
-                    child: Text('Definitions'),
+                  decoration: BoxDecoration(color: Color(0xFF9AC433)),
+                  child: Text('Definitions'),
                 ),
                 ListTile(
                   title: Text('toggle appBar'),
-                  onTap: () { },
+                  onTap: () {},
                 ),
                 ListTile(
                   title: Text('toggle Theme'),
-                  onTap: () { },
+                  onTap: () {},
                 ),
               ],
             ),
@@ -50,14 +48,15 @@ class GenericApp extends StatelessWidget {
           body: new Container(
             margin: const EdgeInsets.all(20.0),
             alignment: Alignment.center,
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                new Container(
-                  child: TextField(
-                    decoration: InputDecoration(
+            child: SingleChildScrollView(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  new Container(
+                    child: TextField(
+                      decoration: InputDecoration(
                         labelText: 'User Name',
                         labelStyle: TextStyle(color: Color(0xFF8091A5)),
                         prefixIcon: Icon(
@@ -68,64 +67,63 @@ class GenericApp extends StatelessWidget {
                         fillColor: Color(0xFFCDD5DD),
                         floatingLabelBehavior: FloatingLabelBehavior.auto,
                         enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(width: 1, color: Color(0xFFCDD5DD)),
+                          borderSide:
+                              BorderSide(width: 1, color: Color(0xFFCDD5DD)),
                           borderRadius: BorderRadius.circular(4),
                         ),
                         focusedBorder: OutlineInputBorder(
                           borderSide: BorderSide(width: 2, color: Colors.blue),
                           borderRadius: BorderRadius.circular(4),
                         ),
+                      ),
+                      // keyboardType: TextInputType.emailAddress,
                     ),
-                    // keyboardType: TextInputType.emailAddress,
                   ),
-                ),
-                PasswordInput(),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10.0),
-                  child: ElevatedButton(
-                    // style: ButtonStyle(
-                    //   backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-                    // ),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.green,
-                        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                        textStyle: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold)),
-                    onPressed: () {},
-                    child: const Text('SIGN IN'),
-                  ),
-                ),
-                new Container(
-                    margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                    child: Text(
-                      'Don\'t have an account?',
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(color: Colors.grey),
-                    )
-                ),
-                new Container(
+                  PasswordInput(),
+                  Container(
                     margin: EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text(
-                      'SIGN UP NOW',
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: const Color(0xFF9AC433)),
-                    )
-                ),
-                new Container(
-                  margin: EdgeInsets.symmetric(vertical: 20.0),
-                  child: Text(
-                    'Terms & Conditions',
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(color: Colors.grey),
-                  )
-                ),
-              ],
+                    child: ElevatedButton(
+                      // style: ButtonStyle(
+                      //   backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                      // ),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.green,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 40, vertical: 15),
+                          textStyle: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold)),
+                      onPressed: () {},
+                      child: const Text('SIGN IN'),
+                    ),
+                  ),
+                  new Container(
+                      margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                      child: Text(
+                        'Don\'t have an account?',
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(color: Colors.grey),
+                      )),
+                  new Container(
+                      margin: EdgeInsets.symmetric(vertical: 10.0),
+                      child: Text(
+                        'SIGN UP NOW',
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: const Color(0xFF9AC433)),
+                      )),
+                  new Container(
+                      margin: EdgeInsets.symmetric(vertical: 20.0),
+                      child: Text(
+                        'Terms & Conditions',
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(color: Colors.grey),
+                      )),
+                ],
+              ),
             ),
           ),
         ),
@@ -133,5 +131,3 @@ class GenericApp extends StatelessWidget {
     );
   }
 }
-
-
