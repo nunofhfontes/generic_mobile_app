bd74d76bd699eb9e86e825768e1cf459a0607745
nunofontes <nunof.h.fontes@gmail.com>
2021-09-09 20:41:24 +0100

    separating Home Page screen in a different Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:generic_app/screens/HomePageScreen.dart';
 import 'package:generic_app/widgets/HomePageDrawer.dart';
 
-import 'package:generic_app/widgets/PasswordInput.dart';
-import 'package:generic_app/widgets/UserNameInput.dart';
 
 void main() {
   runApp(GenericApp());
@@ -30,68 +29,14 @@ class GenericApp extends StatelessWidget {
             elevation: 0,
           ),
           drawer: HomePageDrawer(),
-          body: new Container(
-            margin: const EdgeInsets.all(20.0),
-            alignment: Alignment.center,
-            child: SingleChildScrollView(
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  UserNameInput(),
-                  PasswordInput(),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10.0),
-                    child: ElevatedButton(
-                      // style: ButtonStyle(
-                      //   backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-                      // ),
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.green,
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 40, vertical: 15),
-                          textStyle: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold)),
-                      onPressed: () {},
-                      child: const Text('SIGN IN'),
-                    ),
-                  ),
-                  new Container(
-                      margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                      child: Text(
-                        'Don\'t have an account?',
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(color: Colors.grey),
-                      )),
-                  new Container(
-                      margin: EdgeInsets.symmetric(vertical: 10.0),
-                      child: Text(
-                        'SIGN UP NOW',
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: const Color(0xFF9AC433)),
-                      )),
-                  new Container(
-                      margin: EdgeInsets.symmetric(vertical: 20.0),
-                      child: Text(
-                        'Terms & Conditions',
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(color: Colors.grey),
-                      )),
-                ],
-              ),
-            ),
-          ),
+          body: HomePageScreen(),
         ),
       ),
     );
   }
 }
+
+
 
 
 
--- lib/screens/HomePageScreen.dart
+++ lib/screens/HomePageScreen.dart
@@ -1,3 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:generic_app/widgets/PasswordInput.dart';
+import 'package:generic_app/widgets/UserNameInput.dart';
 
+class HomePageScreen extends StatelessWidget {
+  const HomePageScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.all(20.0),
+      alignment: Alignment.center,
+      child: SingleChildScrollView(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            UserNameInput(),
+            PasswordInput(),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0),
+              child: ElevatedButton(
+                // style: ButtonStyle(
+                //   backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                // ),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 15),
+                    textStyle: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold)),
+                onPressed: () {},
+                child: const Text('SIGN IN'),
+              ),
+            ),
+            new Container(
+                margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: Text(
+                  'Don\'t have an account?',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(color: Colors.grey),
+                )),
+            new Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  'SIGN UP NOW',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFF9AC433)),
+                )),
+            new Container(
+                margin: EdgeInsets.symmetric(vertical: 20.0),
+                child: Text(
+                  'Terms & Conditions',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(color: Colors.grey),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
